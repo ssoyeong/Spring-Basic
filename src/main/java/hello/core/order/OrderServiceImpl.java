4b654e9994490f66ad6a73607e00b7e0358e29db
ssoyeong <jsy171771@gmail.com>
2022-04-10 12:15:30 +0900

    Feat: 다양한 의존관계 주입 방법 테스트
    Lombok 라이브러리의 @RequiredArgsConstructor 사용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,47 +1,33 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-//import hello.core.discount.FixDiscountPolicy;
-//import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
-
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    /*
-    * 위와 같이 변경하는 것은, client인 OverServiceImpl를 고쳐야 한다.
-    * 인터페이스 DiscountPolicy뿐만 아니라 구체적인 클래스도 의존하고 있음
-    * DIP 위반. OCP 위반.
-     */
-
-    private final MemberRepository memberRepository ;
+    private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+    // @RequiredArgsConstructor 로 대체
+    /*
+    // @Autowired   // 생성자가 하나일 때는 @Autowired 생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-
-    /* 인터페이스에만 의존하도록 생성자를 통해 구체화한다. */
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-        /*
-         * 할인에 관한 것은 createOrder가 관여하지 않고
-         * discountPolicy에게 무조건 넘겨버림
-         * SRP를 잘 지킴
-         */
-
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
