3b82b35929f4529240a8a9221dedc3555f101d53
ssoyeong <jsy171771@gmail.com>
2022-04-12 10:17:16 +0900

    Feat: 빈 생명주기 콜백 Method 2
    설정 정보에 초기화 메서드, 종료 메서드 @Bean 지정

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,14 +26,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {     // DI가 끝나면 실행됨
+    public void init() {     // DI가 끝나면 실행됨
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {        // Bean 종료 후 실행됨
+    public void close() {        // Bean 종료 후 실행됨
         disconnect();
     }
 }
